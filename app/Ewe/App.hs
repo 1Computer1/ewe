6810567a11c32e4bdfef328496f63e3a44f530f1
1computer1 <22125769+1Computer1@users.noreply.github.com>
2019-11-16 02:07:08 -0500

    refactor(app): reduce code duplication

--- app/Ewe/App.hs
+++ app/Ewe/App.hs
@@ -10,18 +10,21 @@ import Ewe.App.Repl as X
 import Ewe.App.Types as X
 import Ewe.Language
 
-runProgram :: Language lang => lang -> Source -> IO ()
-runProgram lang source = case parse >>= eval of
+run :: Language lang
+    => (lang -> Source -> Either ParserError a)
+    -> (lang -> a -> Either Error (Val lang))
+    -> lang
+    -> Source
+    -> IO ()
+run parseF evalF lang source = case parse >>= eval of
     Left err -> putStrLn err
     Right x  -> putStrLn $ pretty lang x
     where
-        parse = first prettyParserError $ parseProgram lang source
-        eval = first (prettyError source) . evalProgram lang
+        parse = first prettyParserError $ parseF lang source
+        eval = first (prettyError source) . evalF lang
+
+runProgram :: Language lang => lang -> Source -> IO ()
+runProgram = run parseProgram evalProgram
 
 runExpr :: Language lang => lang -> Source -> IO ()
-runExpr lang source = case parse >>= eval of
-    Left err -> putStrLn err
-    Right x  -> putStrLn $ pretty lang x
-    where
-        parse = first prettyParserError $ parseExpr lang source
-        eval = first (prettyError source) . evalExpr lang (prelude lang)
+runExpr lang = run parseExpr (flip evalExpr (prelude lang)) lang
